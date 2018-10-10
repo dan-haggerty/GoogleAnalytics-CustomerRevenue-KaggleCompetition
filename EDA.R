9b34904fb423adec43d70c5711fc160d6fe1eb65
dan-haggerty <ddhaggerty@ddhagerty-mac.local>
2018-10-10 11:18:45 -0400

    Upate EDA script with new steps for isolating just a few rows of data so I can work with it more easily. Then figure out how to separate out the nested fields/dimensions/variables.

--- EDA.R
+++ EDA.R
@@ -7,45 +7,17 @@ library(ggrepel)
 library(gridExtra)
 library(countrycode)
 
+# load the training data in to the environment
 train <- read_csv("train.csv")
 
-# This is a Kernel from the following discussion
-# It is one way to separate out the individual variables from the columns
-# that have combined them.
-# https://www.kaggle.com/erikbruin/google-analytics-eda-with-screenshots-of-the-app/notebook
+# extract out just the first 10 rows. So I have a smaller
+# data set to work with while I work on extracting out the nested fields
+first10rows <- train[1:10,]
 
-# Keep in mind that the dependent variable "transactionRevenue" only happens for a small
-# subset of the visits. So its only in a few instances of the "totals" column/variable.
-# Also, it doesn't appear that the transactionRevenune variable is in the test set. 
-# Which is what I expected. 
+# separate out the nested values from the device, geoNetwork, totals and trafficSource variables
+separateTotals <- first10rows %>%
+                    separate(totals, into = c("blank", "visitsTitle", "visitsCount", "hitsTitle", "hitsCount", "pageviewTitle",
+                            "pageviewCount", "bouncesTitle", "bouncesCount", "newVisitsTitle", 
+                            "newVisitsCount"))
 
-#JSON columns are "device", "geoNetwork", "totals", "trafficSource"
-
-tr_device <- paste("[", paste(train$device, collapse = ","), "]") %>% fromJSON(flatten = T)
-tr_geoNetwork <- paste("[", paste(train$geoNetwork, collapse = ","), "]") %>% fromJSON(flatten = T)
-tr_totals <- paste("[", paste(train$totals, collapse = ","), "]") %>% fromJSON(flatten = T)
-tr_trafficSource <- paste("[", paste(train$trafficSource, collapse = ","), "]") %>% fromJSON(flatten = T)
-
-te_device <- paste("[", paste(test$device, collapse = ","), "]") %>% fromJSON(flatten = T)
-te_geoNetwork <- paste("[", paste(test$geoNetwork, collapse = ","), "]") %>% fromJSON(flatten = T)
-te_totals <- paste("[", paste(test$totals, collapse = ","), "]") %>% fromJSON(flatten = T)
-te_trafficSource <- paste("[", paste(test$trafficSource, collapse = ","), "]") %>% fromJSON(flatten = T)
-
-#Combine to make the full training and test sets
-train <- train %>%
-  cbind(tr_device, tr_geoNetwork, tr_totals, tr_trafficSource) %>%
-  select(-device, -geoNetwork, -totals, -trafficSource)
-
-test <- test %>%
-  cbind(te_device, te_geoNetwork, te_totals, te_trafficSource) %>%
-  select(-device, -geoNetwork, -totals, -trafficSource)
-
-#Remove temporary tr_ and te_ sets
-rm(tr_device)
-rm(tr_geoNetwork)
-rm(tr_totals)
-rm(tr_trafficSource)
-rm(te_device)
-rm(te_geoNetwork)
-rm(te_totals)
-rm(te_trafficSource)
+#filter the totals row to find one with the word revenue in it.
